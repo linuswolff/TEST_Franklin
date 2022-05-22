fe30c4362a28e0ce64b5b7d3c0e199d0334f1d52
linuswolff <79108813+linuswolff@users.noreply.github.com>
2022-05-22 13:26:04 +0000

    Deploying to gh-pages from  @ 91adc79d90db6a3c2af519418bece45b1766a96a 🚀

---
+++ assets/menu_mine3/test5.jl
@@ -0,0 +1,10 @@
+# This file was generated, do not modify it. # hide
+using RCall
+theme(:ggplot2) #HIDE
+
+R"USmacro <- read.table('./_assets/USmacro.txt',
+                         header = TRUE, dec = ',')"
+
+USmacro = @rget USmacro #HIDE
+first(USmacro, 5)
+println(first(USmacro[:,1:7], 5)) #HIDE
