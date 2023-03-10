462661dfff482a80054601475bcc3f909fcfa1a2
linuswolff <79108813+linuswolff@users.noreply.github.com>
2023-03-10 10:33:56 +0000

    Deploying to gh-pages from  @ 4a16072291e331a456d97f3b9493315a3fabf8ee 🚀

--- __site/assets/menu_mine/test2.jl
+++
@@ -1,4 +0,0 @@
-# This file was generated, do not modify it. # hide
-theme(:ggplot2)
-p = plot(seq, erg_form4, size = (700,500))
-savefig(p, "plot.svg")
--- __site/assets/menu_mine6/test8.jl
+++
@@ -1,7 +0,0 @@
-# This file was generated, do not modify it. # hide
-using RCall
-
-R"
-library(xfun)
-xfun::embed_file("resume_test.pdf")
-"
