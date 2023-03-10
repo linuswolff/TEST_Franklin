1f1311d964c2cd93f7f18eb7bcc63eaf3b6c4e22
linuswolff <79108813+linuswolff@users.noreply.github.com>
2023-03-10 20:09:11 +0000

    Deploying to gh-pages from  @ 112d5e734720a39f388312de186ac4cac0672ec4 🚀

---
+++ assets/straßenschilder/code/_ceval_2.jl
@@ -0,0 +1,4 @@
+# This file was generated, do not modify it. # hide
+#hideall
+println("1/1 [==============================] - 0s 23ms/step
+This sign belongs to class: Speed limit (30km/h)")
---
+++ assets/straßenschilder/code/_ceval_4.jl
@@ -0,0 +1,4 @@
+# This file was generated, do not modify it. # hide
+#hideall
+println("1/1 [==============================] - 0s 20ms/step
+This sign belongs to class: Roundabout mandatory")
